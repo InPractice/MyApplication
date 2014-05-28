27979206f56019149f2a0a7486607b3ca8b56a55
zhulin <soufunwuxian@julius.local>
2014-05-28 13:19:50 +0800

    first commit

---
+++ MyApplication/MyApplication/JTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JTTAppDelegate.h
+//  MyApplication
+//
+//  Created by zhulin on 14-5-25.
+//  Copyright (c) 2014年 MyTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyApplication/MyApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyApplication
+//
+//  Created by zhulin on 14-5-25.
+//  Copyright (c) 2014年 MyTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JTTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JTTAppDelegate class]));
+    }
+}
