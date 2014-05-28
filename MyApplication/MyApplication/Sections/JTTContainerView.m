736d3eda2981553139fd9a42c024a0b4487fe4e8
zhulin <soufunwuxian@julius.local>
2014-05-28 13:31:23 +0800

    创建一个容器视图

---
+++ MyApplication/MyApplication/Sections/JTTContainerView.m
@@ -0,0 +1,31 @@
+//
+//  JTTContainerView.m
+//  MyApplication
+//
+//  Created by zhulin on 14-5-28.
+//  Copyright (c) 2014年 MyTeam. All rights reserved.
+//
+
+#import "JTTContainerView.h"
+
+@implementation JTTContainerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
